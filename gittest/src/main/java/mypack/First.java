c94ec3d24f97c8061a995f970306ae44c5fa63af
bingdazi <841664196@qq.com>
2018-04-20 17:36:40 +0800

    仓库初始化代码提交

---
+++ gittest/src/main/java/mypack/First.java
@@ -0,0 +1,15 @@
+package mypack;
+
+/**
+ * @desc github工程的第一个类文件
+ * @date 2018年4月20日
+ * @version v1.0
+ */
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("this is the first class file from github!");
+		System.out.println("github工程的第一个类文件!");
+	}
+}
